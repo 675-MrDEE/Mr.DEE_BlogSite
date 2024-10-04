1bd06640a47a4b18f1917b02d1beeb643e718a4b
Mr. DEE <675mrdee@gmail.com>
2024-10-04 10:53:59 +1000

    links4sale site
    links4sale site added

---
+++ links4sale/script.js
@@ -0,0 +1,23 @@
+/* tabs for the navbar menu */
+
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+    target.classList.add("active");
+  });
+});
+
+/* page refresh button */
+/* document.getElementById("refreshButton").addEventListener("click", function () {
+  
+  location.reload();
+}); */
